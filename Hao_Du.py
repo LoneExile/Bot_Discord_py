efc4c8822600ee14e7381aaf5c940fd24a526442
Wolowit <Apinant-Moo@tutanota.com>
2021-04-22 07:48:56 +0700

    add requests and lock channel

--- Hao_Du.py
+++ Hao_Du.py
@@ -1,5 +1,54 @@
 import discord
 import requests
-import TOKEN
+import D_TOKEN
+import json
 
-print("Hello")
+
+client = discord.Client()
+
+
+def get_quotes():
+    response = requests.get("https://zenquotes.io/api/random")
+    json_data = json.loads(response.text)
+    for quote_give in json_data:
+        quote = f"{quote_give['q']}\n by {quote_give['a']}"
+        # print(type(json_data))
+        # print(json_data[0]["q"])
+    return quote
+
+
+#  get_quotes()
+
+
+@client.event
+async def on_ready():
+    print("We have logged in as {0.user}".format(client))
+
+
+@client.event
+async def on_message(message):
+    #  don't respond to ourselves
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("$hello"):
+        quote = get_quotes()
+        await message.channel.send(f"{quote} ")
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    print("Message from {0.author}: {0.content}".format(message))
+    #  lock user channel to command bot
+
+    channel_lock = "bot"
+
+    if str(message.channel) == channel_lock:
+        await message.channel.send("!play here")
+    elif "{0.content}".format(message) == "!play":
+        await message.delete()
+
+
+client.run(D_TOKEN.TOKEN)
